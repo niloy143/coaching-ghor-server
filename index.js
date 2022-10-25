e0228942e514021aef2995a10b0213f35172f4a9
niloy143 <niloymahmudapu@gmail.com>
2022-10-25 11:49:42 +0600

    sample fake data

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = process.env.port || 5000;
 
 app.use(cors())
 
+const courses = require('./courses.json')
+
 app.get('/', (req, res) => {
     res.send('Coaching Ghor Server Is Running...')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
